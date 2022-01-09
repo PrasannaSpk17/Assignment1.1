71b25c92688229ef31293124001c0219f8d65ade
PrasannaSpk17 <prasannaspk1717@gmail.com>
2022-01-09 20:30:13 +0545

    first assignment

---
+++ swapusingpassbref.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+void swap(int &a,int &b){
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
+int main(){
+    int a,b;
+    cout<<"enter two numbers to swap"<<endl;
+    cin>>a>>b;
+    swap(a,b);
+    cout<<"after swap"<<a<<b;
+}
